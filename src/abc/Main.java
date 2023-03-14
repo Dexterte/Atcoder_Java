953962c1dcf18aafe7074540e33b88a0dfbdc333
Dexterte <rejimully@gmail.com>
2023-03-14 20:49:26 +0900

    テンプレの作成

--- src/abc/Main.java
+++ src/abc/Main.java
@@ -1,7 +1,18 @@
 package abc;
 
+import java.io.PrintWriter;
+import java.util.Scanner;
+
 public class Main {
+
+    static PrintWriter pw = new PrintWriter(System.out);
+
     public static void main(String[] args) {
+
+	Scanner scanner = new Scanner(System.in);
+
+	scanner.close();
+	pw.close();
 
     }
 
